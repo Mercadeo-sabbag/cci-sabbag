950c6dcdb9784386bbc888ffe1de2a1778973e73
= <=>
2026-02-25 08:31:42 -0500

    vercel adapter to correct deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
 import node from "@astrojs/node"
+
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
